47312b294a7ef1e34e8601dc537e9c842679b434
futuredeveloper41 <mj10283@naver.com>
2024-12-16 11:22:57 +0900

    커밋 메시지

---
+++ src/main/java/com/spring/mvcproject/MvcprojectApplication.java
@@ -0,0 +1,14 @@
+package com.spring.mvcproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcprojectApplication {
+
+	public static void main(String[] args) {
+		// jsp 연결하는 거 다 되어 있는 것
+		SpringApplication.run(MvcprojectApplication.class, args);
+	}
+
+}
